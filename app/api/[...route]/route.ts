e8a1a91e6dc433a7e0d5ca5563ac425d7c08d97f
isntboxs <isntboxs@gmail.com>
2025-04-09 16:04:41 +0700

    feat: add @vercel/speed-insights and support additional HTTP methods
    Add @vercel/speed-insights package to monitor performance metrics and include SpeedInsights component in the layout. Additionally, support POST, PUT, DELETE, PATCH, and OPTIONS methods in the API route handler to enhance functionality.

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,8 +1,6 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
-export const runtime = "edge";
-
 const app = new Hono().basePath("/api");
 
 app.get("/hello", (c) => {
@@ -12,3 +10,8 @@ app.get("/hello", (c) => {
 });
 
 export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const DELETE = handle(app);
+export const PATCH = handle(app);
+export const OPTIONS = handle(app);
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "@/styles/globals.css";
 
 const geistSans = Geist({
@@ -29,6 +31,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				{children}
+				<SpeedInsights />
 			</body>
 		</html>
 	);
