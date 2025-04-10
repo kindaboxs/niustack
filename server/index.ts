6b96e03ec99d1855f22858df3a416a9760a92b4c
MrBoxs <rizkywanda200155@gmail.com>
2025-04-10 10:46:36 +0700

    feat(server): add CORS middleware to handle cross-origin requests
    The CORS middleware is added to the server to allow cross-origin requests from the specified origin. This ensures secure and controlled access to the API endpoints, supporting necessary headers, methods, and credentials.

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,24 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
+import { env } from "@/env";
 import notFound from "@/server/middlewares/not-found";
 import onError from "@/server/middlewares/on-error";
 import { authRoute } from "@/server/routers/auth-router";
 
 const app = new Hono().basePath("/api");
+
+app.use(
+	"*",
+	cors({
+		origin: env.NEXT_PUBLIC_APP_URL,
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	})
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route("/auth", authRoute);
